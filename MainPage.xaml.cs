be5ac9f62c98edabe7f33c98366a53156d23c4eb
yzbai <yzbai@brlite.com>
2022-06-22 07:51:14 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private async void TestBtn_Clicked(object sender, EventArgs e)
+    {
+        HttpClient httpClient = new HttpClient();
+        string anyUriWillbeOk = "https://github.com/";
+        HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, anyUriWillbeOk);
+
+        requestMessage.Content = new StringContent("{}", Encoding.UTF8, "application/json");
+        
+        HttpResponseMessage responseMessage = await httpClient.SendAsync(requestMessage);
+    }
+}
+
